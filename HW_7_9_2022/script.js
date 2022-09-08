d8b61771c884cfef9c74574423f1b2266eacab19
Ben <selahben@hotmail.com>
2022-09-08 15:16:44 +0300

    HW_7_9_2022-4th

--- HW_7_9_2022/script.js
+++ HW_7_9_2022/script.js
@@ -1,3 +1,5 @@
+//Option 1
+
 window.onload = function () {
   let chessBoard = [];
   let boardRow = [];
@@ -20,3 +22,36 @@ window.onload = function () {
   }
   console.table(chessBoard);
 };
+
+//Option 2
+
+// window.onload = function () {
+//   let chessBoard = [];
+//   let boardRow = [];
+//   let x, y;
+//   let color1 = "Black";
+//   let color2 = "White";
+//   let colorTemp;
+//   let id;
+//   let myCol;
+//   for (x = 0; x < 8; x++) {
+//     boardRow = [];
+//     for (y = 0; y < 4; y++) {
+//       boardRow.push(color1);
+//       boardRow.push(color2);
+//     }
+//     chessBoard.push(boardRow);
+//     colorTemp = color1;
+//     color1 = color2;
+//     color2 = colorTemp;
+//   }
+//   console.table(chessBoard);
+
+//   for (x = 0; x < 8; x++) {
+//     for (y = 0; y < 8; y++) {
+//       id = "myCol_" + x + "_" + y;
+//       myCol = document.getElementById(id);
+//       myCol.style.backgroundColor = chessBoard[x][y];
+//     }
+//   }
+// };
